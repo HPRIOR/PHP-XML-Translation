b7794d0b3f39af8c75e6e0a16cdfac3cc75ef41e
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-04-14 23:38:52 +0100

    table working

--- main.php
+++ main.php
@@ -1,6 +1,7 @@
 <?php
 
 function queryString($medium, $titleType, $venue, $authorType, $pid){
+    //change query - think 'book' medium type is the problem
     if ($venue != ""){
         return "//dblpperson/r/{$medium}[{$titleType}/text()=\"{$venue}\"]/{$authorType}[@pid=\"{$pid}\"]";
     }
@@ -9,51 +10,82 @@ function queryString($medium, $titleType, $venue, $authorType, $pid){
     }
 }
 
-function queryXML()
+function getNameArray(){
+    $nameArray = [];
+    foreach($_GET as $key => $value){
+        if ($key != "venue") {
+            $nameArray[] = $key;
+        }
+    }
+    return $nameArray;
+}
+
+function countArray()
 {
-    $textinput = end($_GET);
-    $nameCountArray = [];
+    $textInput = end($_GET);
+    $nestedArray = [];
+    $countArray = [];
     //cycle through each xml document with names given
-    foreach ($_GET as $key => $value)
-    {
-        if ($key != "venue")
-        {
-            $countArray = [];
+    foreach ($_GET as $key => $value) {
+        if ($key != "venue") {
             $xmldoc = new DOMDocument();
             $xmldoc->load($key . ".xml");
             $xpath = new DOMXPath($xmldoc);
             // cycle through again for each name given
-            foreach ($_GET as $key2 => $value2)
-            {
-                if ($key != "venue")
-                {
+            foreach ($_GET as $key2 => $value2) {
+                if ($key2 != "venue"){
                     $domList = $xpath->query(
-                        queryString("article",  "journal", $textinput, "author", $value2)."|".
-                        queryString("book",  "none", $textinput, "author", $value2)."|".
-                        queryString("inproceedings",  "booktitle", $textinput, "author", $value2) ."|".
-                        queryString("incollection",  "booktitle", $textinput, "author", $value2) ."|".
-                        queryString("proceedings",  "booktitle", $textinput, "editor", $value2)
+                        queryString("article",  "journal", $textInput, "author", $value2)."|".
+                        queryString("book",  "none", $textInput, "author", $value2)."|".
+                        queryString("inproceedings",  "booktitle", $textInput, "author", $value2) ."|".
+                        queryString("incollection",  "booktitle", $textInput, "author", $value2) ."|".
+                        queryString("proceedings",  "booktitle", $textInput, "editor", $value2)
                     );
-                    echo count($domList, COUNT_NORMAL);
-                    echo " ";
+                    $countArray[] = count($domList, COUNT_NORMAL);
                 }
             }
+            $nestedArray[] = $countArray;
+            $countArray = [];
         }
     }
+
+    return $nestedArray;
 }
-//$i; $i < count($_Get,COUNT_NORMAL)-1; $i++
 
-
-
+function createTable(){
+    $nestedArray = countArray();
+    $count = count($nestedArray, COUNT_NORMAL);
+    $namearray = getNameArray();
+    echo "<tr>";
+    echo "<th> -  </th>";
+    foreach ($_GET as $key => $value) {
+        if ($key != "venue") echo "<th>{$key}</th>";
+    }
+    echo "</tr>";
+    for ( $i = 0 ; $i < $count; $i++){
+        echo "<tr>";
+        echo "<td>".$namearray[$i]."</td>";
+        for ( $j = 0 ; $j < $count; $j++){
+            echo "<td>";
+            echo $nestedArray[$i][$j];
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
+}
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
-<body>
-<?php
-queryXML();
+    <body>
+        <header>
 
-?>
-</body>
+        </header>
+        <table>
+            <?php
+            createTable();
+            ?>
+        </table>
+    </body>
 </html>
