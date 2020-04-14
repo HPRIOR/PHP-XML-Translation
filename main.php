abd6525b664da6608f70d5688299fb003ae9e9b6
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-04-14 16:47:11 +0100

    queries are working

--- main.php
+++ main.php
@@ -1,12 +1,58 @@
+<?php
+
+function queryString($medium, $titleType, $venue, $authorType, $pid){
+    if ($venue != ""){
+        return "//dblpperson/r/{$medium}[{$titleType}/text()=\"{$venue}\"]/{$authorType}[@pid=\"{$pid}\"]";
+    }
+    else {
+        return "//dblpperson/r/{$medium}/{$authorType}[@pid=\"{$pid}\"]";
+    }
+}
+
+function queryXML()
+{
+    $textinput = end($_GET);
+    $nameCountArray = [];
+    //cycle through each xml document with names given
+    foreach ($_GET as $key => $value)
+    {
+        if ($key != "venue")
+        {
+            $countArray = [];
+            $xmldoc = new DOMDocument();
+            $xmldoc->load($key . ".xml");
+            $xpath = new DOMXPath($xmldoc);
+            // cycle through again for each name given
+            foreach ($_GET as $key2 => $value2)
+            {
+                if ($key != "venue")
+                {
+                    $domList = $xpath->query(
+                        queryString("article",  "journal", $textinput, "author", $value2)."|".
+                        queryString("book",  "none", $textinput, "author", $value2)."|".
+                        queryString("inproceedings",  "booktitle", $textinput, "author", $value2) ."|".
+                        queryString("incollection",  "booktitle", $textinput, "author", $value2) ."|".
+                        queryString("proceedings",  "booktitle", $textinput, "editor", $value2)
+                    );
+                    echo count($domList, COUNT_NORMAL);
+                    echo " ";
+                }
+            }
+        }
+    }
+}
+//$i; $i < count($_Get,COUNT_NORMAL)-1; $i++
+
+
+
+?>
 
 
 <!DOCTYPE html>
 <html lang="en">
 <body>
 <?php
-foreach ($_GET as $key => $value){
-    echo $key;
-}
+queryXML();
 
 ?>
 </body>
